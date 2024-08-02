03b5e9582a3c9d65035131a624cf1f1db1d97039
Alberto Rovellini <alberto.rovellini@gmail.com>
2024-08-02 16:47:03 -0700

    added code to compare catchtot and catch nc in space

--- compare_Atlantis_catch_outputs.R
+++ compare_Atlantis_catch_outputs.R
@@ -1,6 +1,15 @@
 # Alberto Rovellini
 # 08/02/2024
 # Compare flat text to nc files for catch output
+# This is needed if we want to use CATCH.nc for the plotting and calibration
+# The Problem:
+# I want to use CATCH.nc for plots and to calculate calibration factors 
+# CATCH.nc is the only file that has catch output by species, fleet, and box
+# This is what Isaac thinks about this file:
+# I always start with the simple CatchPerFishery.txt (which is not spatial). I trust that the most.  Then I check to see which NC files agree with it.   (I have to admit I have not played with these catch NC files in a while, and I forgot one might in units of numbers). My recollection is that whatever  NC file gave us catch per species per polygon (but not per fleet) was correct; but that no NC file correctly gave catch per species per polygon per fleet.    I can dig around more in old files, but I definitely recommend using the txt files as (spatially aggregated) truth.
+# So, if we want to use, we need to:
+# 1. Compare catch as FC vars (t) from the nc file to the flat txt file - not spatial
+# 2. Compare catch by box between CATCH.nc and CATCHTOT.nc (we have done this before)
 
 run <- 1574 # no fleets
 
@@ -111,7 +120,7 @@ catch_nc_FC <- bind_rows(catch_nc_ls)
 # collapse space for comparison with txt
 catch_nc_FC_nospace <- catch_nc_FC %>%
   group_by(ts, fleet, Name) %>%
-  summarise(mt = sum(mt), na.rm = T) %>%
+  summarise(mt = sum(mt, na.rm = T)) %>%
   left_join(grps %>% select(Code, Name)) %>%
   rename(year = ts) %>%
   select(year, fleet, Code, mt)
@@ -131,3 +140,73 @@ hist(catch_comp$ratio, breaks = 100)
 # unfortunately, comparisons by box cannot be done (the txt file is spatially aggregated)
 # no txt files for catch are by box
 # We can compare catch in space between the two nc files
+
+# Compare CATCH.nc to CATCHTOT.nc -----------------------------------------
+
+# now nc file
+catch_nc_tot_ls <- list()
+
+for(n in 1:length(all_fg)){
+  fg <- all_fg[n] # this needs to use the "Name" to pull from the NC file
+  this_tidync <- tidync(catch_nc_file_TOT)
+  this_nc <- ncdf4::nc_open(catch_nc_file_TOT)
+  
+  code <- grps %>% filter(Name == fg) %>% pull(Code)
+  
+  #Extract from the output .nc file the appropriate catch time series variables
+  catch_vars <- this_tidync %>%
+    activate("D1,D0") %>%
+    hyper_vars() %>% # all variables in the .nc file active grid
+    filter(grepl("Tot_.*_Catch",name)) %>% # filter for reserve N
+    filter(grepl(code,name)) # filter for specific functional group
+  
+  # catch_vars <- catch_vars %>%
+  #   mutate(Index = as.numeric(gsub(paste0(code, "_Catch_FC"), "", name))) %>%
+  #   left_join(fleet_key %>% dplyr::select(Code, Index))
+  
+  catch <- purrr::map(catch_vars$name,ncdf4::ncvar_get,nc=this_nc) 
+  
+  catch_df <- as.data.frame((catch))
+  
+  # add box_id
+  catch_df <- catch_df %>% mutate(box_id = 0:108)
+  
+  # reshape
+  catch_df_long <- catch_df %>%
+    pivot_longer(-box_id, names_to = "ts", values_to = "mt")
+  
+  # turn ts column to integer
+  catch_df_long <- catch_df_long %>%
+    mutate(ts = gsub("X","",ts)) %>%
+    mutate(ts = as.numeric(ts)) %>%
+    mutate(ts = ts - 1) # start numbering ts from 0
+  
+  catch_box_df <- catch_df_long
+  
+  # drop ts = 0
+  catch_box_df <- catch_box_df %>%
+    filter(ts > 0)
+  
+  # add species
+  catch_box_df <- catch_box_df %>%
+    mutate(Name = fg)
+  
+  catch_nc_tot_ls[[n]] <- catch_box_df
+  
+}
+
+catch_nc_TOT <- bind_rows(catch_nc_tot_ls)
+
+# aggregate fleets in FC frame
+catch_nc_FC_nofleets <- catch_nc_FC %>%
+  group_by(ts, box_id, Name) %>%
+  summarise(mt = sum(mt, na.rm = T))
+
+# compare
+catch_comp_nc <- catch_nc_TOT %>%
+  left_join(catch_nc_FC_nofleets, by = c('ts','box_id','Name')) %>%
+  mutate(ratio = mt.x/mt.y)
+
+summary(catch_comp_nc$ratio)
+
+# the two nc files are identical in their reported catch by box
