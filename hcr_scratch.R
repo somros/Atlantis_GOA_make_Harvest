16d5711b7e53cec54e53e39dc7f7d562762b74f5
Alberto Rovellini <alberto.rovellini@gmail.com>
2025-06-06 13:48:20 -0700

    made function to pull data faster

--- hcr_scratch.R
+++ hcr_scratch.R
@@ -21,7 +21,7 @@ codes <- grps %>% pull(Code)
 
 # species and fleets
 # pull these from a reference OY run - comparing them makes sense only if all species are consistent
-ref_run <- 2060
+ref_run <- 2097
 oy_dir <- paste0("C:/Users/Alberto Rovellini/Documents/GOA/Parametrization/output_files/data/out_", ref_run)
 harvest_prm <- list.files(oy_dir)[grep("GOA_harvest_.*.prm", list.files(oy_dir))]
 harvest <- readLines(paste(oy_dir, harvest_prm, sep = "/"))
@@ -46,18 +46,22 @@ for(sp in codes){
 # Still debating on the best way to do this - typing in here or reading in an excel sheet. Pros and cons to both
 # model runs
 #run <- c(2060, 2061, 2064, 2065, 2066, 2067, 2072, 2073, 2074, 2075)
-run <- c(2072, 2073, 2074, 2075, 2080, 2081, 2082, 2083)
+#run <- c(2072, 2073, 2074, 2075, 2080, 2081, 2082, 2083)
+run <- c(2097:2112)
 
 # caps
 #cap <- c(400, 200, 600, 400, 200, 600, NA, 400, 200, 600) * 1000
-cap <- rep(c(NA, 400, 200, 600), 2) * 1000
+#cap <- rep(c(NA, 400, 200, 600), 2) * 1000
+cap <- rep(c(200,400,600,NA), 4) * 1000
 
 # weight scheme
 #wgts <- c(rep("equal", 3), rep("binary", 4), rep("ramp", 3))
-wgts <- c(rep("ramp", 8))
+#wgts <- c(rep("ramp", 8))
+wgts <- c(rep("equal", 8), rep("binary", 8))
 
 # climate forcings
-env <- c(rep(1999, 4), rep("2014", 4))
+# env <- c(rep(1999, 4), rep("2014", 4))
+env <- rep(c(rep("ssp126", 4), rep("ssp585", 4)), 2)
 
 # combine all run factors in a label key
 set_key <- function(run, cap = NULL, wgts = NULL, env = NULL, other = NULL) { # gamma = NULL, 
@@ -81,94 +85,114 @@ key_config <- set_key(run, cap, wgts, env)
 # function to extract fishery info for stocks that are part of the OY
 pull_fishery_info <- function(this_run){
   
+  print(this_run)
+  
+  # File paths
   wd <- paste0("C:/Users/Alberto Rovellini/Documents/GOA/Parametrization/output_files/data/out_", this_run)
   biom_file <- paste0("outputGOA0", this_run, "_testAgeBiomIndx.txt")
   catch_file <- paste0("outputGOA0", this_run, "_testCatch.txt")
   harvest_prm <- list.files(wd)[grep("GOA_harvest_.*.prm", list.files(wd))]
   ecocap_file <- paste0(wd, "/outputGOA0", this_run, "_test_EcosystemCapResult.txt")
   
+  # Read files once
   biom <- read.csv(paste(wd, biom_file, sep = "/"), sep = " ", header = T)
   catch <- read.csv(paste(wd, catch_file, sep = "/"), sep = " ", header = T)
   harvest <- readLines(paste(wd, harvest_prm, sep = "/"))
   ecocap_report <- read.delim(ecocap_file, sep = " ")
   
-  res_df <- data.frame()
-  for(sp in oy_species){
+  # Process biomass data once, outside the loop
+  # Convert to long format once and use faster string splitting
+  biom_long <- biom %>%
+    pivot_longer(-Time, names_to = "Code.Age", values_to = "mt")
+  
+  # Use faster string splitting method
+  # Split Code.Age using base R (much faster than separate())
+  code_age_split <- strsplit(biom_long$Code.Age, "\\.", fixed = FALSE)
+  biom_long$Code <- sapply(code_age_split, `[`, 1)
+  biom_long$Age <- as.numeric(sapply(code_age_split, `[`, 2))
+  
+  # Pre-filter to only relevant species and times
+  biom_filtered <- biom_long %>%
+    filter(Code %in% oy_species,
+           Time > 0, Time < max(Time)) %>%
+    select(-Code.Age)  # Remove original column
+  
+  # Pre-process harvest parameters outside loop
+  harvest_params <- list()
+  for(sp in oy_species) {
+    startage_line <- harvest[grep(paste0(sp, "_mFC_startage"), harvest) + 1]
+    startage <- as.numeric(strsplit(startage_line, split = " ")[[1]])[1]
     
-    sp_idx <- grep(sp, codes)
-    
-    # get start age for selex
-    startage <- as.numeric(unlist(strsplit(harvest[grep(paste0(sp, "_mFC_startage"), harvest)+1], split = " ")))[1]
-    
-    # reference points: pull them from PRM if the species is managed with an HCR
-    estbo <- NA
-    if(sp %in% hcr_spp){
-      # get estbo
-      estbo_vec <- as.numeric(unlist(strsplit(harvest[grep("estBo\t", harvest)+1], split = " ")))
+    if(sp %in% hcr_spp) {
+      sp_idx <- grep(sp, codes)
+      estbo_vec <- as.numeric(strsplit(harvest[grep("estBo\t", harvest) + 1], split = " ")[[1]])
       estbo <- estbo_vec[sp_idx]
-      
-      # get Fref
-      fref_vec <- as.numeric(unlist(strsplit(harvest[grep("Fref\t", harvest)+1], split = " ")))
-      fref <- -log(1 - fref_vec[sp_idx]) # turn to real F as there were entered as mu
-      
-    } else { # fref for non-HCR species is from mFC
-      
-      mfc <- as.numeric(unlist(strsplit(harvest[grep(paste0("mFC_",sp," "), harvest)+1], split = " ")))[1]
-      fref <- -(365) * log(1-mfc)
-      
+      fref_vec <- as.numeric(strsplit(harvest[grep("Fref\t", harvest) + 1], split = " ")[[1]])
+      fref <- -log(1 - fref_vec[sp_idx])
+    } else {
+      estbo <- NA
+      mfc_line <- harvest[grep(paste0("mFC_", sp, " "), harvest) + 1]
+      mfc <- as.numeric(strsplit(mfc_line, split = " ")[[1]])[1]
+      fref <- -(365) * log(1 - mfc)
     }
     
-    # biomass and catch
-    # selected biomass
-    biom_selex <- biom %>%
-      pivot_longer(-Time, names_to = "Code.Age", values_to = "mt") %>%
-      separate(`Code.Age`, into = c("Code", "Age"), sep = "\\.") %>%
-      filter(Code == sp) %>%
-      filter(Age >= startage) %>%
-      group_by(Time) %>%
-      summarise(biom_mt_selex = sum(mt))
-    
-    # total biomass for B/B0
-    biom_tot <- biom %>%
-      pivot_longer(-Time, names_to = "Code.Age", values_to = "mt") %>%
-      separate(`Code.Age`, into = c("Code", "Age"), sep = "\\.") %>%
-      filter(Code == sp) %>%
-      group_by(Time) %>%
-      summarise(biom_mt_tot = sum(mt))
-    
-    # catch
-    catch_sp <- catch %>% select(Time, all_of(sp)) %>% rename(catch_mt = sp)
-    
-    # put it all together
-    tmp <- left_join(biom_selex, catch_sp, by = "Time") %>% 
-      left_join(biom_tot, by = "Time") %>%
-      filter(Time > 0, Time < max(Time)) %>% # drop first and last time step
-      mutate(mu = catch_mt / biom_mt_selex, # selected exploitation rate
-             f = -log(1-mu), # selected F, as emerging from Atlantis
-             biom_frac = biom_mt_tot / estbo, # biomass fraction, computed with total biomass because that is what Atlantis uses by default
-             fref = fref,
-             Code = sp,
-             run = this_run)
-    
-    res_df <- rbind(res_df, tmp)
-    
+    harvest_params[[sp]] <- list(startage = startage, estbo = estbo, fref = fref)
   }
   
-  # pull the oy rescaling factor
-  # remove col in excess
-  ecocap_report <- ecocap_report %>%
-    select(Time, SpeciesName, FisheryName, SpBased_Frescale, PostSystCap_Frescale)
+  # Process all species at once using vectorized operations
+  # Create biomass summaries for all species
+  biom_selex_all <- biom_filtered %>%
+    left_join(
+      data.frame(Code = names(harvest_params),
+                 startage = sapply(harvest_params, `[[`, "startage")),
+      by = "Code"
+    ) %>%
+    filter(Age >= startage) %>%
+    group_by(Time, Code) %>%
+    summarise(biom_mt_selex = sum(mt), .groups = 'drop')
   
-  # filter and compute rescaling, then thin out
+  biom_tot_all <- biom_filtered %>%
+    group_by(Time, Code) %>%
+    summarise(biom_mt_tot = sum(mt), .groups = 'drop')
+  
+  # Process catch data for all species
+  catch_long <- catch %>%
+    select(Time, all_of(oy_species)) %>%
+    pivot_longer(-Time, names_to = "Code", values_to = "catch_mt")
+  
+  # join operation
+  result_list <- list()
+  for(sp in oy_species) {
+    sp_params <- harvest_params[[sp]]
+    
+    sp_data <- biom_selex_all %>%
+      filter(Code == sp) %>%
+      left_join(filter(catch_long, Code == sp), by = c("Time", "Code")) %>%
+      left_join(filter(biom_tot_all, Code == sp), by = c("Time", "Code")) %>%
+      mutate(
+        mu = catch_mt / biom_mt_selex,
+        f = -log(1 - mu),
+        biom_frac = biom_mt_tot / sp_params$estbo,
+        fref = sp_params$fref,
+        run = this_run
+      )
+    
+    result_list[[sp]] <- sp_data
+  }
+  
+  # OPTIMIZATION 7: Single rbind instead of iterative rbind
+  res_df <- do.call(rbind, result_list)
+  
+  # Process ecocap report
   ecocap_report <- ecocap_report %>%
+    select(Time, SpeciesName, FisheryName, SpBased_Frescale, PostSystCap_Frescale) %>%
     filter(SpeciesName %in% oy_species,
            FisheryName %in% oy_fleets) %>%
-    mutate(Time = Time,
-           oy_rescale = PostSystCap_Frescale / SpBased_Frescale) %>%
+    mutate(oy_rescale = PostSystCap_Frescale / SpBased_Frescale) %>%
     select(Time, SpeciesName, oy_rescale) %>%
     rename(Code = SpeciesName)
   
-  # tie to other info
+  # Final join
   res_df <- res_df %>%
     left_join(ecocap_report, by = c("Time", "Code"))
   
@@ -227,11 +251,13 @@ plot_fishery <- function(catch_df){
     
     p_tmp <- catch_df %>%
       filter(Code == current_code) %>%
+      filter(!is.na(catch_mt)) %>%
       mutate(f_frac = f / fref) %>%
       select(-fref, -biom_mt_tot, -biom_mt_selex, -mu) %>%
       pivot_longer(-c(Time, Code, Name, run, cap, wgts, env, other)) %>%
       ggplot(aes(x = Time/365, y = value, color = factor(cap), linetype = factor(env))) +
       geom_line() +
+      scale_y_continuous(limits = c(0,NA)) +
       facet_grid2(wgts ~ name, scales = "free_y", independent = "y") +
       labs(title = current_name,
            x = "Year", 
@@ -246,7 +272,7 @@ plot_fishery <- function(catch_df){
       ) 
     
     ggsave(paste0(plotdir, "/by_spp/", current_code, ".png"), p_tmp, 
-           width = 8, height = 5,  
+           width = 12, height = 5,  
            units = "in", dpi = 300)
     
   }
@@ -260,35 +286,36 @@ plot_fishery <- function(catch_df){
     
     # TODO: sort out faceting - you want the interesting feature to be in the same panel (e.g., facet by weight scheme or by climate scenario?)
     p2 <- catch_df %>%
-      filter(Code == current_code, Time >= 10*365) %>%
-      ggplot(aes(x = biom_frac, y = f, color = Time/365))+
+      filter(Code == current_code, Time >= 10*365, !is.na(catch_mt)) %>%
+      ggplot(aes(x = biom_frac, y = f/fref, color = Time/365))+
       geom_point(aes(shape = factor(wgts)), size = 2)+
       scale_shape_manual(values = c(1:length(unique(catch_df$wgts))))+
       scale_color_viridis_c(option = "inferno")+
       geom_vline(xintercept = 0.4, linetype = "dashed", color = "blue", linewidth = 1)+
       geom_vline(xintercept = 0.02, linetype = "dashed", color = "red", linewidth = 1)+
       geom_vline(xintercept = 0.2, linetype = "dotted", color = "orange", linewidth = 1)+
-      geom_hline(aes(yintercept = fref), linetype = "dashed", linewidth = 1, color = "steelblue")+
+      geom_hline(yintercept = 1, linetype = "dashed", linewidth = 1, color = "steelblue")+
       scale_y_continuous(limits = c(0,NA))+
       theme_bw()+
       labs(x = "B/B0", 
-           y = "F", 
+           y = "F/Ftarg", 
            color = "Year",
            shape = "Weight scheme")+
       facet_grid(factor(env)~cap)
     
-    ggsave(paste0(plotdir, "/", "hcr.png"), p2, 
-           width = 8, height = 5, 
+    ggsave(paste0(plotdir, "/", current_code, "_hcr.png"), p2, 
+           width = 12, height = 5, 
            units = "in", dpi = 300)
     
   }
   
-  
   # ecocap rescaling
   p3 <- catch_df %>%
-    ggplot(aes(x = Time / 365, y = oy_rescale, color = wgts))+
+    filter(!is.na(catch_mt)) %>%
+    ggplot(aes(x = Time / 365, y = oy_rescale, color = env, linetype = wgts))+
     geom_line()+
     theme_bw()+
+    labs(x = "Year", y = "OY-based catch rescaling")+
     facet_grid(Code~cap)
   
   ggsave(paste0(plotdir, "/", "rescale_factor.png"), p3, 
@@ -297,7 +324,7 @@ plot_fishery <- function(catch_df){
   
 }
 
-plot_fishery(catch_df)
+plot_fishery(catch_df_fast)
 
 
 
